2f943d2a3f1d872e3131bc1cfc02ef18d702e1e5
Netot <netot228@yandex.ru>
2024-11-21 21:49:00 +0300

    переименовать app-root->app

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/app-root/app';
+import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
