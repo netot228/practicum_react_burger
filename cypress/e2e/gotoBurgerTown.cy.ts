4ade7feb2d4449e1fdae4835740cda7130ca3c83
Netot <netot228@yandex.ru>
2025-02-19 21:49:53 +0300

    cy start

--- cypress/e2e/gotoBurgerTown.cy.ts
+++
@@ -1,5 +0,0 @@
-describe("CHECK burgerTown is available", function () {
-    it("CHECK burgerTown should be available on localhost:3000", function () {
-        cy.visit("http://localhost:3000/");
-    });
-});
